da5b803324d573d36674b98d59dd7fcb8a8e951c
cocokui <dengyanyu@hotmail.com>
2015-07-27 21:19:30 +0800

    Site updated: 2015-07-27 21:19:30

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 (function($){
-  // Nav bar toggle
-  $('#main-nav-toggle').on('click', function(){
-    $('.nav-container-inner').slideToggle();
+  // Search
+  var $searchWrap = $('#search-form-wrap'),
+    isSearchAnim = false,
+    searchAnimDuration = 200;
+
+  var startSearchAnim = function(){
+    isSearchAnim = true;
+  };
+
+  var stopSearchAnim = function(callback){
+    setTimeout(function(){
+      isSearchAnim = false;
+      callback && callback();
+    }, searchAnimDuration);
+  };
+
+  $('#nav-search-btn').on('click', function(){
+    if (isSearchAnim) return;
+
+    startSearchAnim();
+    $searchWrap.addClass('on');
+    stopSearchAnim(function(){
+      $('.search-form-input').focus();
+    });
+  });
+
+  $('.search-form-input').on('blur', function(){
+    startSearchAnim();
+    $searchWrap.removeClass('on');
+    stopSearchAnim();
   });
 
   // Share
@@ -79,8 +106,4 @@
     $('.fancybox').fancybox();
   }
 
-  //Back to top
-  $("#back-to-top").on('click', function(){  
-    $('body,html').animate({scrollTop:0}, 600);
-  }); 
 })(jQuery);
